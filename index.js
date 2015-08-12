73cd1c8615017cd6632c022ce435fb06d79813df
Rob Loach <robloach@gmail.com>
2015-08-12 11:22:49 -0400

    Add 'html' to HTML-Minifier `inputFormats`
    Allows HTML to be processed through the minifier.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var minify = require('html-minifier').minify;
 var merge = require('merge');
 
 exports.name = 'html-minifier';
-exports.inputFormats = ['htmlmin', 'html-minifier'];
+exports.inputFormats = ['htmlmin', 'html-minifier', 'html'];
 exports.outputFormat = 'html';
 
 exports.render = function (str, options, locals) {
