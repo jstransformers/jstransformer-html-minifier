098b21d05b2b18e6c70ceeff2154f14b03f41f8c
Rob Loach <robloach@gmail.com>
2018-06-28 09:43:42 -0400

    Fix xo syntax

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const minify = require('html-minifier').minify
+const minify = require('html-minifier')
 const extend = require('extend-shallow')
 
 exports.name = 'html-minifier'
@@ -8,5 +8,5 @@ exports.inputFormats = ['htmlmin', 'html-minifier', 'html']
 exports.outputFormat = 'html'
 
 exports.render = function (str, options, locals) {
-  return minify(str, extend({}, options, locals))
+  return minify.minify(str, extend({}, options, locals))
 }
